440b974c10cf7949ed3cb64910a8f29221ea91f0
nicokupferscayla <58943551+nicokupferscayla@users.noreply.github.com>
2019-12-17 13:12:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ if (window.location.href.match(jira______scayla)) {
  */
 function runCode______scayla() {  
   if (!document.querySelectorAll("#create-issue-dialog").length) {
+    document.getElementById('create_link').click();
     check_check_check_check______scayla = setInterval(() => { 
       if (document.querySelectorAll('#create-issue-dialog').length) {      
-        document.getElementById('create_link').click();
         clearInterval(check_check_check_check______scayla);
         setTypeBug______scayla();
       }
