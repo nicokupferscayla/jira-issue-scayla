64abfc5dc3b741c99053c9685d540ed74908abc8
nicokupferscayla <58943551+nicokupferscayla@users.noreply.github.com>
2019-12-17 12:43:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ function runCode______scayla() {
   // TODO: add an If that says you need to open the popup first.
   if (!jQuery("#create-issue-dialog")) {
     timeout = 2000;
+    console.log('create link?')
     document.getElementById('create_link').click()
   }
   
